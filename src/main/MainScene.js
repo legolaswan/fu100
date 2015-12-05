2f252bd515c2f25cfab1d6630cc534d315d7d495
shenhj <hj_shen@163.com>
2015-12-05 10:19:01 +0800

    changes

--- src/main/MainScene.js
+++ src/main/MainScene.js
@@ -1,8 +1,11 @@
 /**
- * Created by Administrator on 2015/11/27.
  * 主场景
  */
 
 var MainScene = cc.Scene.extend({
-
+	onEnter:function(){
+		this._super();
+		var menuLayer = new MainMenuLayer();
+		this.addChild(menuLayer, 0, 1);
+	}
 });
