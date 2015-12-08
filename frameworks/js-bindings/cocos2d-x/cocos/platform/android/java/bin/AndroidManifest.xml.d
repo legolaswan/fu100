7eec16fa2dd8fec25a5f09873a54eb479c642ffe
shenhj <hj_shen@163.com>
2015-12-08 19:26:17 +0800

    changes

---
+++ frameworks/js-bindings/cocos2d-x/cocos/platform/android/java/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+D:\cocos2d-js-v3.0\frameworks\js-bindings\cocos2d-x\cocos\platform\android\java\bin\AndroidManifest.xml : \
+D:\cocos2d-js-v3.0\frameworks\js-bindings\cocos2d-x\cocos\platform\android\java\AndroidManifest.xml \
